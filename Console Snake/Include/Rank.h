e1aff954d654760e2eeb324536cabc14449249a4
James Taylor <james.taylor_rw@outlook.com>
2022-02-05 17:35:24 +0800

    make Rank.h not header-only

--- Console Snake/Include/Rank.h
+++ Console Snake/Include/Rank.h
@@ -10,8 +10,6 @@
 #include <future>
 #include <mutex>
 #include <shared_mutex>
-#include <algorithm>
-#include <cassert>
 
 class RankBase
 {
@@ -28,78 +26,18 @@ public:
 	static constexpr auto name_max_length = 12;
 
 protected:
-	RankBase() noexcept
-	{
-		try {
-			rank_table.reserve(rank_count + 1);
-		}
-		catch (...) {
-			// Strong exception guarantee of vector.reserve
-		}
-	}
+	RankBase() noexcept;
 
 public:
-	void newResult(std::wstring new_name, int new_score, bool winning)
-	{
-		if (done.valid()) // wait for last time sorting
-			done.get();
-
-		RankItem new_one = {
-			std::move(new_name),
-			new_score,
-			GameSetting::get().speed.Value(),
-			GameSetting::get().width,
-			GameSetting::get().height,
-			winning
-		};
-
-		done = std::async(std::launch::async,
-						  [this, new_one = std::move(new_one)]
-						  {
-							  auto [rank, lock] = this->modifyRank();
-							  rank.push_back(new_one);
-
-							  auto previous_user = std::find_if(rank.begin(), rank.end(),
-																[&](const RankItem& lhs) noexcept
-																{
-																	return lhs.name == rank.back().name;
-																});
-							  auto end = rank.end();
-							  assert(previous_user != end);
-							  // find and store previous named gamer's best score
-							  if (previous_user != rank.cend() - 1 && !rank.back().name.empty())
-							  {
-								  if (rank.back().score >= previous_user->score)
-									  *previous_user = std::move(rank.back());
-								  --end;
-							  }
-							  std::stable_sort(rank.begin(), end,
-											   [](const RankItem& lhs, const RankItem& rhs) noexcept
-											   {
-												   return lhs.score > rhs.score;
-											   });
-							  rank.pop_back();
-						  });
-	}
+	void newResult(std::wstring new_name, int new_score, bool winning);
 
 	std::pair<const std::vector<RankItem>&, std::shared_lock<std::shared_mutex>>
-	getRank() const
-	{
-		return { rank_table, std::shared_lock{ rank_mutex } };
-	}
+	getRank() const;
 
 	std::pair<std::vector<RankItem>&, std::unique_lock<std::shared_mutex>>
-	modifyRank()
-	{
-		return { rank_table, std::unique_lock{ rank_mutex } };
-	}
+	modifyRank();
 
-	void clearRank()
-	{
-		std::unique_lock lock(rank_mutex);
-		rank_table.clear();
-		rank_table = std::vector<RankItem>{ rank_count };
-	}
+	void clearRank();
 
 private:
 	std::vector<RankItem> rank_table{ rank_count };
